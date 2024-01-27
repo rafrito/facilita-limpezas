cca145a2861d6a9ece4e5fddb07b6e230b83748b
Rafael Mateus <r.mateusmarques@gmail.com>
2024-01-27 05:23:01 -0300

    Adicionado verificação de payload às rotas

--- backend/routes/clientes.js
+++ backend/routes/clientes.js
@@ -7,7 +7,13 @@ const router = express.Router();
 router.post('/cadastro', async (req, res) => {
   try {
     const { nome, email, telefone } = req.body
+    if (!nome || !email || !telefone) {
+      return res.status(400).send({ message: "Nome, email e telefone são obrigatórios." });
+    }
     const body = await cadastroCliente(nome, email, telefone);
+    if (body.inserted = false) {
+      return res.status(409).send(body);
+    }
     res.json(body);
   } catch (error) {
     console.error(error)
@@ -18,6 +24,9 @@ router.post('/cadastro', async (req, res) => {
 router.post('/endereco', async (req, res) => {
   try {
     const { email, eixo_x, eixo_y } = req.body
+    if (!email || !eixo_x || !eixo_y) {
+      return res.status(400).send({ message: "Email e as coodenadas são obrigatórios." });
+    }
     const body = await cadastroEndereco(email, eixo_x, eixo_y);
     res.json(body);
   } catch (error) {
@@ -29,6 +38,9 @@ router.post('/endereco', async (req, res) => {
 router.post('/list', async (req, res) => {
   try {
     const { pagina, tamanhoPagina, termoBusca } = req.body;
+    if (termoBusca === undefined) {
+      return res.status(400).send({ message: "O termo de busca é obrigatório." });
+    }
     const clientes = await buscarClientesPaginado(pagina, tamanhoPagina, termoBusca);
     res.json(clientes);
   } catch (error) {
